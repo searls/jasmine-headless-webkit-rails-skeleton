8c61121a0594ab4b4eb18de6eacb00a12ed27137
Justin Searls <searls@gmail.com>
2011-08-19 11:03:03 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineDemo::Application.config.secret_token = '383a232ec9168304eab6fa6f86fcba9ba7b32fd656f74d77fec16c4c96656e72547493642b6a96e45382f4b662a2c7ed58af915fd148e5ec8e09af42fce41010'
