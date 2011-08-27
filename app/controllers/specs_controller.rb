23f5ba3d80ef76844482e68f9a8d80c8fc574dd5
Justin Searls <searls@gmail.com>
2011-08-26 23:22:11 -0400

    You can now visit http://localhost:3000/specs/index.html to see your specs in a browser. not entirely DRY, unfortunately, and all of the specs are inlined into script tags. Sub-optimal, for sure :-/

---
+++ app/controllers/specs_controller.rb
@@ -0,0 +1,2 @@
+class SpecsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 JasmineDemo::Application.routes.draw do
+  get "specs/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
